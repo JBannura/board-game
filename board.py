fc04fed01fcd71e12a2f6248d7e185ea1b9304a8
guicavalheiro <cavalheiro.dev@gmail.com>
2020-10-18 17:32:23 -0300

    updating board

--- board.py
+++ board.py
@@ -10,6 +10,7 @@
 """
 import pygame
 import random
+import time
 
 class Game:
     
@@ -23,7 +24,7 @@ class Game:
         pygame.display.set_caption("Array Backed Grid")
         
         # Criando o dado
-        self.dice = [Button("Roll", 300, 77.5, (255, 255, 255))]
+        self.dice = [Button("Roll", 300, 77, (255, 255, 255))]
         
         # Dimensoes
         self.width  = width    
@@ -59,9 +60,110 @@ class Game:
     # Dice Roll
     def dice_roll(self):
         diceRoll = random.randint(1, 6)
-        print(diceRoll)
+        print(f"Rolou: {diceRoll}")
         return diceRoll
     
+    def board_coloring(self):
+        
+        for row in range(self.x_grid):
+            for column in range(self.y_grid):
+                
+                if column == 0 or column == 11 or row == 0 or row == 9:
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+                
+                elif column == 2 and row != 8:    
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+                
+                elif row == 7 and column != 1 and column != 10:
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+                
+                elif column == 9 and row != 1 and row != 8:
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+                
+                elif row == 2 and column != 1 and column != 3 and column != 10:
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+                
+                elif column == 4 and row != 1 and row != 6 and row != 8:
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+                
+                elif row == 5 and column != 1 and column != 3 and column != 8 and column != 10:
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+                
+                elif row == 4 and column != 1 and column != 3 and column != 5 and column != 8 and column != 10:
+                    casa = self.grid[row][column]
+                    casa.color = (94, 191, 226)
+        
+    def create_path(self):
+        self.path = []
+        
+        # 1: [1, 1] - [8, 1]
+        for x in range(1, 9):
+            self.path.append(self.grid[x][1])
+        
+        # 2: [8, 2] - [8, 10]
+        for y in range(2, 11):
+            self.path.append(self.grid[8][y])
+        
+        # 3: [7, 10] - [1, 10] 
+        for x in range(7, 0, -1):
+            self.path.append(self.grid[x][10])
+        
+        # 4: [1, 9] - [1, 3]
+        for y in range(9, 2, -1):
+            self.path.append(self.grid[1][y])
+        
+        # 5: [2, 3] - [6, 3]
+        for x in range(2, 7):
+            self.path.append(self.grid[x][3])
+        
+        # 6: [6, 4] - [6, 8]
+        for y in range(4, 9):
+            self.path.append(self.grid[6][y])
+        
+        # 7: [5, 8] - [3, 8]
+        for x in range(5, 2, -1):
+            self.path.append(self.grid[x][8])
+        
+        # 8: [3, 7] - [3, 5]
+        for y in range(7, 4, -1):
+            self.path.append(self.grid[3][y])
+        
+        # 9: [4, 5]
+        self.path.append(self.grid[4][5])
+        
+        # Setando cores da posição inicial e final do tabuleiro
+        self.path[-1].color  = (235, 203, 0)
+        self.path[0].color = (164, 164, 164)
+        
+        dict_ = {}
+        for index in range(0, 24):
+            
+            casa = random.randint(1, 46)
+    
+            if len(dict_) > 0:
+                try:
+                    if casa in dict_:
+                        while casa in dict_:
+                            casa = random.randint(1, 46)
+                        
+                except KeyError:
+                    pass
+                
+            if index >= 12:
+                self.path[casa].bad_place()
+            
+            else:
+                self.path[casa].good_place()
+            
+            dict_[casa] = casa
+        
 class Button:
     
     # Button creation
@@ -79,7 +181,7 @@ class Button:
         pygame.draw.rect(screen, self.button_color, (self.button_x, self.button_y, self.button_width, self.button_height)) 
         font = pygame.font.SysFont('comicsans', 20)
         text = font.render(self.button_text, 1, (0, 0, 0))
-        screen.blit(text, (self.button_x + round(self.button_width/2) - round(text.get_width()/2), self.button_y + round(self.button_height/2) - round(text.get_height()/2)))
+        screen.blit(text, (self.button_x + int(self.button_width/2) - int(text.get_width()/2), self.button_y + int(self.button_height/2) - int(text.get_height()/2)))
 
     # Dice click
     def click(self, pos):
@@ -109,33 +211,51 @@ class Casa:
 
 class Player:
     
-    def __init__(self, player_id):
-        
-        #print(casa)
+    def __init__(self, player_id, xi, yi):
         
         self.width  = 5
         self.height = 5
+        self.x = xi
+        self.y = yi
         
         if player_id == '0':    
             self.color = (0, 0, 0)
+            self.player_id = 0
 
-        self.posicao_atual = [0, 0]
+        self.posicao_atual = 0
         
-    def atualiza_posicao(self, resultado, screen, grid):
+        self.message()
         
-        # 10 - 10
-        pos_x = self.posicao_atual[0]
-        pos_y = self.posicao_atual[1]
+    def atualiza_posicao(self, resultado, path):
+        self.posicao_atual += resultado
         
-        next_pos = ''
+        if self.posicao_atual > 47:
+            self.posicao_atual = 47
+            
+        casa = path[self.posicao_atual]
         
-        self.draw()
-
-    def draw(self, screen, casa):
         self.x = casa.x1
         self.y = casa.y1
+        #print(f"X: {self.x}")
+        #print(f"Y: {self.y}")
+
+        self.message()
         
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height)) 
+        return self.posicao_atual 
+        
+    def message(self):
+        
+        if self.posicao_atual == 0:
+            print("\nInício do jogo!")
+        
+        elif self.posicao_atual == 47:
+            print("\nFim do jogo!")
+        
+        else:
+            print("\nRole o dado novamente!")        
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
         #font = pygame.font.SysFont('comicsans', 20)
         #text = font.render(self.button_text, 1, (0, 0, 0))
         #screen.blit(text, (self.button_x + round(self.button_width/2) - round(text.get_width()/2), self.button_y + round(self.button_height/2) - round(text.get_height()/2)))
@@ -156,18 +276,20 @@ HEIGHT = 20
 MARGIN = 5
 
 # Y grid
-y_grid = 10
+y_grid = 12
 
 # X grid
-x_grid = 12
+x_grid = 10
 
 # Creating Game
 game = Game(WIDTH, HEIGHT, MARGIN, x_grid, y_grid) 
 game.create_screen(500, 255)
 game.create_board()
- 
+game.board_coloring()
+game.create_path()
+
 # Player
-player_1 = Player('0')
+player_1 = Player('0', game.path[0].x1, game.path[0].y1)
 game.add_player(player_1)
 
 # Loop until the user clicks the close button.
@@ -177,6 +299,8 @@ done = False
 clock = pygame.time.Clock()
 
 # -------- Main Program Loop -----------
+start = True
+posicao = 0
 while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
@@ -190,14 +314,13 @@ while not done:
             for dice in game.dice:
                 if dice.click(pos):
                     resultado = game.dice_roll()
-                    player_1.atualiza_posicao(resultado)
- 
+                    posicao = player_1.atualiza_posicao(resultado, game.path)
+                    
     # Set the screen background
     game.screen.fill(BACKGROUND_COLOR)
- 
+    
     # Draw the grid
     for row in range(x_grid):
-        print(row)
         for column in range(y_grid):
             casa = game.grid[row][column]
             pygame.draw.rect(game.screen,
@@ -213,8 +336,10 @@ while not done:
     
     # Displaying players
     for player in game.player_list:
-        player.draw(game.screen, game.grid[0][0])
-             
+        player.draw(game.screen)
+        if posicao == 47:
+            done = True
+            
     # Limit to 60 frames per second
     clock.tick(60)
              
@@ -222,4 +347,5 @@ while not done:
     pygame.display.update()
              
 # Be IDLE friendly. If you forget this line, the program will 'hang' on exit.
+time.sleep(2)
 pygame.quit()
